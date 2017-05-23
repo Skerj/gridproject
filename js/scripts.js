135c11c84b49bc2b5725faabc177fe206f2991f4
John Mooney <jrmint46@gmail.com>
2017-05-22 20:47:40 -0400

    Added for loop for grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-$(document).ready(function() {
-	for (i = 0; i < 5; i++) {
-		$('tr').after($('.gridelement'));
+$(document).ready(function () {
+	var b=16;
+	var c=b*b;
+	for (var a=0; a<c; a++) {
+		$('#container').append("<div class='box'></div>")
 	}
 });
