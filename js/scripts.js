36ac466739c6291d1a3bbe6e5ae3a5b93427161c
John Mooney <jrmint46@gmail.com>
2017-05-23 17:21:12 -0400

    Added prompt button functionality for the grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
-	var b=16;
-	var c=b*b;
-	for (var a=0; a<c; a++) {
-		$('#container').append("<div class='box'></div>")
+
+	function newGrid() {
+		var askGrid = prompt("Enter dimension for grid", 16);
+		var b=askGrid;
+		var c=b*b;
+		for (var a=0; a<c; a++) {
+			$('#container').append("<div class='box'></div>")
+		}
+		$('.box').on('mouseenter', function() {
+				$(this).addClass('hover');
+		});
 	}
-	$('.box').on('mouseenter', function() {
-			$(this).addClass('hover');
+
+	$('#clearall').on('click', function() {
+		$('.box').remove()
+		$(newGrid)
 	});
-});
-
-$('#clearall').on('click', function () {
 
 });
+
