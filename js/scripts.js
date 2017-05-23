36100b5299f52e0d9394654eaed772a444ca816b
John Mooney <jrmint46@gmail.com>
2017-05-23 18:02:04 -0400

    Added height/width functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
 $(document).ready(function () {
 
+
+
 	function newGrid() {
 		var askGrid = prompt("Enter dimension for grid", 16);
 		var b=askGrid;
 		var c=b*b;
+
 		for (var a=0; a<c; a++) {
 			$('#container').append("<div class='box'></div>")
 		}
+		squareSize();
+
+		function squareSize() {
+			var square = 860 / askGrid
+			$('.box').css('height', square + 'px');
+			$('.box').css('width', square + 'px');
+		}
+
 		$('.box').on('mouseenter', function() {
 				$(this).addClass('hover');
 		});
+
+
 	}
 
 	$('#clearall').on('click', function() {
